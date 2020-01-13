5cbab0ccbdba4ed00d612b9370b86e2ba8654d26
Riley Slayden <slaydenriley@gmail.com>
2020-01-13 16:16:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,18 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
