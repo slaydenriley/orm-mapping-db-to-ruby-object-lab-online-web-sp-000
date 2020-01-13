53d38b91058b162031a2808cbe9b0fd39a6c2d47
Riley Slayden <slaydenriley@gmail.com>
2020-01-13 16:16:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,11 +66,12 @@ class Student
       SELECT *
       FROM students
       WHERE grade < 10
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
   def save
