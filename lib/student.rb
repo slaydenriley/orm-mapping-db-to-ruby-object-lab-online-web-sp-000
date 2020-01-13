b910d6b40f2ed8fd93740015dacbc5d64f31afeb
Riley Slayden <slaydenriley@gmail.com>
2020-01-13 16:15:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,10 @@ class Student
       FROM students
       WHERE grade = 9
     SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.students_below_12th_grade
